ad7c4286008926164397a58d4734feb81de1ae9e
Chris Armata <chris.armata@gmail.com>
2019-03-16 01:04:02 -0400

    Add train schedule functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,3 +8,50 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
+
+$(document).ready(function() {
+  database.ref().on("child_added", function(snapshot) {
+    var data = snapshot.val();
+
+    var tr = $("<tr>");
+    tr.append($("<th>").text(data.name));
+    tr.append($("<th>").text(data.destination));
+    tr.append($("<th>").text(data.frequency));
+
+    var minutesAway = Math.abs(
+      moment().diff(moment(data.firstTrain, "HH:mm"), "minutes") %
+        data.frequency
+    );
+    var nextArrival = moment()
+      .add(minutesAway, "minutes")
+      .format("hh:mm A");
+
+    tr.append($("<th>").text(nextArrival));
+    tr.append($("<th>").text(minutesAway));
+
+    $("tbody").append(tr);
+  });
+
+  $("#submit").on("click", function(e) {
+    e.preventDefault();
+    var name = $("#name")
+      .val()
+      .trim();
+    var destination = $("#destination")
+      .val()
+      .trim();
+    var firstTrain = $("#first-train")
+      .val()
+      .trim();
+    var frequency = $("#frequency")
+      .val()
+      .trim();
+
+    database.ref().push({
+      name: name,
+      destination: destination,
+      firstTrain: firstTrain,
+      frequency: frequency
+    });
+  });
+});
