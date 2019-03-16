206acbc3eba7f339899aca35ee5a24200eefa685
Chris Armata <chris.armata@gmail.com>
2019-03-15 23:29:06 -0400

    Initialize Firebase

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+var config = {
+  apiKey: "AIzaSyDju7BtCJ_FI-vDt6-HFsUmuuUk79CbpMk",
+  authDomain: "train-scheduler-f0d0f.firebaseapp.com",
+  databaseURL: "https://train-scheduler-f0d0f.firebaseio.com",
+  projectId: "train-scheduler-f0d0f",
+  storageBucket: "train-scheduler-f0d0f.appspot.com",
+  messagingSenderId: "171218627913"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
